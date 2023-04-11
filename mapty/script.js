7ab83216072f93b33a093d8d431af29f30f40375
unknown <munyaarmel61@gmail.com>
2023-04-11 16:52:56 +0200

    mapty project

---
+++ mapty/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+class App{}
+let mapEvent,map
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      console.log(`https://www.google.com/maps/@-${latitude},${longitude},12z`);
+       map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+         
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+          map.on('click', function (mapE) {
+              mapEvent=mapE
+              form.classList.remove('hidden')
+              inputDistance.focus()
+      });
+    },
+    function () {
+      alert('location not found');
+    }
+  );
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+     const { lat, lng } = mapEvent.latlng;
+     L.marker({ lat, lng })
+       .addTo(map)
+       .bindPopup(
+         L.popup({
+           maxWidth: 250,
+           minWidth: 100,
+           autoClose: false,
+           closeOnClick: false,
+           className: 'running-popup',
+         })
+       )
+       .openPopup()
+       .setPopupContent('workout');
+})
+inputType.addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
